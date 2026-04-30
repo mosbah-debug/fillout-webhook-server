e6328206f627aad1b28c2bdfc483180394ca966f
Mosbah <mosbah@thepeakfp.com>
2026-04-30 16:12:11 +0300

    Temporary: continue past cutoff date for backfill

--- server.js
+++ server.js
@@ -234,7 +234,7 @@ async function syncInProgress() {
 
       for (const sub of responses) {
         if (existingIds.has(sub.submissionId)) continue;
-if (new Date(sub.submissionTime) < new Date("2026-03-01T00:00:00.000Z")) { break; }
+if (new Date(sub.submissionTime) < new Date("2026-03-01T00:00:00.000Z")) { continue; }
         all.push({
   formId: FILLOUT_FORM_ID, formName: "Fillout Form",
   submissionId: sub.submissionId,
